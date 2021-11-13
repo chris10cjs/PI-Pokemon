ce51e6d4fd986d3f232a09b6aad3c8b09c4a339b
Christian Silvero <63545412+chris10cjs@users.noreply.github.com>
2021-11-13 11:08:55 -0300

    fix force bd

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ const axios = require("axios");
 const URL = "https://pokeapi.co/api/v2/type/";
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false}).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
 
